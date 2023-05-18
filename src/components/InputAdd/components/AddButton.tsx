a18630b3020e290ce2e0701bd4b3958f8e0161e0
Anderson David <andersondavidfreitas@gmail.com>
2023-05-18 00:50:37 -0300

    Create add button

---
+++ src/components/InputAdd/components/AddButton.tsx
@@ -0,0 +1,14 @@
+import React, { MouseEvent } from "react";
+
+type onClickFunction = { onClick: (event: MouseEvent) => void };
+
+export default function AddButton(props: onClickFunction) {
+	return (
+		<button
+			onClick={props.onClick}
+			className="text-6xl bg-green-500 w-20 h-20 rounded-3xl pb-2"
+		>
+			+
+		</button>
+	);
+}
