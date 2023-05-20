a9209d56867aa556b10bcb98a8167915a7298bf4
Anderson David <andersondavidfreitas@gmail.com>
2023-05-20 01:10:51 -0300

    Create a tests to TodoComponent

---
+++ src/components/ContainerTodos/components/TodoComponent/index.spec.tsx
@@ -0,0 +1,41 @@
+import { queryByText, render } from "@testing-library/react";
+import TodoComponent from "./index";
+
+describe("<TodoComponent ... />", () => {
+	const mockData = {
+		title: "My TodoComponet",
+		_id: "1",
+		tasks: [{ content: "My task component", isComplete: true, _id: "1" }],
+	};
+
+	afterEach(() => {});
+	it("should render the title", () => {
+		const { getByText } = render(
+			<TodoComponent
+				_id={mockData._id}
+				tasks={mockData.tasks}
+				title={mockData.title}
+			/>
+		);
+
+		expect(getByText("My TodoComponet")).toBeInTheDocument();
+	});
+
+	it("should render the whole component unless the title", () => {
+		const { queryByText } = render(
+			<TodoComponent _id={mockData._id} tasks={mockData.tasks} />
+		);
+		expect(queryByText("My TodoComponet")).not.toBeInTheDocument();
+	});
+
+	it("should set the first task to true", () => {
+		const { getAllByRole } = render(
+			<TodoComponent
+				_id={mockData._id}
+				tasks={mockData.tasks}
+				title={mockData.title}
+			/>
+		);
+		expect(getAllByRole("checkbox")[1]).toBeChecked();
+	});
+});
