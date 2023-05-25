6bd9ee5de359f658400c3ab12e358afae53af0f6
Anderson David <andersondavidfreitas@gmail.com>
2023-05-24 22:28:58 -0300

    Add title to the document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import InputAdd from "@/components/InputAdd";
 import ContainerTodo from "@/components/ContainerTodos";
+import Head from "next/head";
 
 export default function Home() {
-
 	return (
 		<main className={`flex h-screen flex-col items-center justify-between`}>
+			<Head>
+				<title>Simples App de Tarefas</title>
+				<meta property="og:title" content="Simples App de Tarefas" key="title" />
+			</Head>
 			<ContainerTodo />
 			<InputAdd />
 		</main>
