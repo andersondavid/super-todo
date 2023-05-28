ee3bb7b79726c33101b7fcb806b0f1b52540df10
Anderson David <andersondavidfreitas@gmail.com>
2023-05-27 22:54:27 -0300

    Add ActionType to changeDataTodo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import { useReducer } from "react";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { ContextDataTodo, DataTodoInitial } from "@/context/DataTodoContext";
-import {reducerContext, actionType} from '@/context/reducerContext'
+import { reducerContext } from "@/context/reducerContext";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const [dataTodo, dispatchTodo] = useReducer(reducerContext, DataTodoInitial);
 
-	const changeDataTodo = (action: actionType) => {
+	const changeDataTodo = (action: ActionType) => {
 		dispatchTodo(action);
 	};
 
